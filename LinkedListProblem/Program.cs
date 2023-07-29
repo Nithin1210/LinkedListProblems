d3a0672f29276b5e7f111e6a8f6c9a0442963bbc
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-07-29 10:22:10 +0530

    [Nithin] Add : Upadtion of Queue

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -5,16 +5,15 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("LinkedList Problem");
-        Console.WriteLine("Welcome to the Linked List Stack");
-        LinkedListStack stack = new LinkedListStack();
-        stack.Push(56);
-        stack.Push(30);
-        stack.Push(70);
-        stack.Display();
-        stack.Peek();
-        stack.Pop();
-        stack.Display();
+        Console.WriteLine("Here is Queue Fuction");
+        LinkedListQueue queue = new LinkedListQueue();
+        queue.Enqueue(56);
+        queue.Enqueue(30);
+        queue.Enqueue(70);
+        queue.Display();
+        queue.Dequeue();
+        queue.Display();
+
 
     }
 }
